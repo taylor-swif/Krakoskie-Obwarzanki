8fadaf6c9bc5e09c14eeab2f940df0b05872f559
stylub <stylub0@gmail.com>
2024-03-16 16:49:24 +0100

    cos dziala a cos nie

--- backend/schema/schemas.py
+++ backend/schema/schemas.py
@@ -1,3 +1,4 @@
+from distance_calc import distance
 def get_flavors(flavors):
     return flavors.split(';')
 
@@ -10,13 +11,27 @@ def get_flavors(flavors):
 
 def invidual_serial(shop) -> dict:
     return {
-        "id" : str(shop["user_id"]),
+        "id" : str(shop["_id"]),
+        "name" : shop["name"],
         "longitude": float(shop["longitude"]),
         "latitude": float(shop["latitude"]),
-        "payment_methods" : int(shop["payment_methods"]),
+        "payment_methods" : shop["payment_methods"],
         "flavors": get_flavors(shop["flavors"])
     }
     
 def list_serial(shops) -> list:
-    return[invidual_serial(shop) for shop in shops]
+    list_of_dicts = [invidual_serial(shop) for shop in shops]
+    dictionary = {}
+    for shop in list_of_dicts:
+        dictionary[shop["id"]] = (shop["latitude"],shop["longitude"]) 
+    return dictionary
 
+def filter_by_distance(shops,localization,radius = 5):
+    return get_shops_data(shops,distance(list_serial(shops),localization,radius=radius))
+
+def filter_n_nearest(shops,localization,n):
+    return distance(list_serial(shops),localization,number_of_output=n)
+
+def get_shops_data(list_of_shops,filtered_shops):
+    data = [list_of_shops[key] for key in filtered_shops.keys()]
+    return [{"id": key, "distance": filtered_shops[key],"name": shop } for key, shop in data.items()]
