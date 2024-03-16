8725975c6b60872533957ab024100190090c8962
stylub <stylub0@gmail.com>
2024-03-16 17:07:27 +0100

    post is working get not

--- backend/schema/schemas.py
+++ backend/schema/schemas.py
@@ -1,6 +1,4 @@
 from distance_calc import distance
-def get_flavors(flavors):
-    return flavors.split(';')
 
 
 # longitude: str # x coordinate
@@ -15,11 +13,12 @@ def invidual_serial(shop) -> dict:
         "name" : shop["name"],
         "longitude": float(shop["longitude"]),
         "latitude": float(shop["latitude"]),
-        "payment_methods" : shop["payment_methods"],
-        "flavors": get_flavors(shop["flavors"])
+        "card_payment" : shop["card_payment"],
+        "flavors": shop["flavors"]
     }
     
 def list_serial(shops) -> list:
+    print(shops)
     list_of_dicts = [invidual_serial(shop) for shop in shops]
     dictionary = {}
     for shop in list_of_dicts:
