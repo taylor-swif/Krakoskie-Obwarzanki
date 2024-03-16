a89476c254f93849a41763d6a502269134f2f44b
root <root@DESKTOP-I4ERDE3>
2024-03-16 17:47:09 +0100

    a

--- backend/config/database.py
+++ backend/config/database.py
@@ -3,5 +3,6 @@ from pymongo import MongoClient
 MONGO_URL = "mongodb+srv://mongoconnect:123@cluster0.dxzzzjx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 client = MongoClient(MONGO_URL)
 database = client.shop
+database2 = client.user
 collection = database["shops"]
-users_collection = database["users"]
+users_collection = database2["users"]
